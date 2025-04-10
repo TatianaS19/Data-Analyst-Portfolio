032495d4b4a87910590efb58aeeb680596494f7d
TatianaS19 <tsvinoboeva@yandex.ru>
2025-04-10 20:05:51 +0900

    Update RetentionRate.sql
    Signed-off-by: TatianaS19 <tsvinoboeva@yandex.ru>

--- Retention_Rate/RetentionRate.sql
+++ Retention_Rate/RetentionRate.sql
@@ -1,5 +1,5 @@
 select user_id, toString(start_date) as start_date, toString(date) as date, age,city,country,os,source from
-#рассматриваем пользователей, которые впервые воспрользовались приложением 20 дней назад
+#рассматриваем пользователей, которые впервые воспользовались приложением 20 дней назад
 #таблица feed_actions это активность пользователей
 (select user_id, min(toDate(time)) as start_date
 from simulator_20250120.feed_actions
@@ -8,7 +8,7 @@ having start_date>=today()-20
 ) t1
 
 join
-
+#узнаем и приджойним их дальнейшую активность
 (select distinct user_id, toDate(time) as date, age,city,country,os,source
 from simulator_20250120.feed_actions) t2
 
