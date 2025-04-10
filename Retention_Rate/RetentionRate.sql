f8bccb377c11f7df0d2dabcea010dba099c54353
TatianaS19 <tsvinoboeva@yandex.ru>
2025-04-10 19:38:14 +0900

    Update RetentionRate.sql

--- Retention_Rate/RetentionRate.sql
+++ Retention_Rate/RetentionRate.sql
@@ -1,1 +1,14 @@
+select user_id, toString(start_date) as start_date, toString(date) as date, age,city,country,os,source from
 
+(select user_id, min(toDate(time)) as start_date
+from simulator_20250120.feed_actions
+group by user_id 
+having start_date>=today()-20
+) t1
+
+join
+
+(select distinct user_id, toDate(time) as date, age,city,country,os,source
+from simulator_20250120.feed_actions) t2
+
+using user_id
